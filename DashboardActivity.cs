d0a1879627c9da7f1f27b64f8e31f7bd2b8910bc
kaveri1896t <kaveri1896t@gmail.com>
2019-07-23 19:48:58 +0530

    23

--- DashboardActivity.cs
+++ DashboardActivity.cs
@@ -12,21 +12,106 @@ using Android.Widget;
 
 namespace FundooWalkin
 {
+    /// <summary>
+    /// Dashboard showing the candidates selected, to be determined and rejected 
+    /// </summary>
     [Activity(Label = "DashboardActivity")]
     public class DashboardActivity : Activity
     {
-        Button _dateSelectButton;
+        private DateTime currentDate;
+        private Button _dateSelectButton;
+        private Spinner spinner;
+        private Button SelectedButton;
+        private Button TBDButton;
+        private Button RejectedButton;
+        private TextView TxtViewCurrentDate;
+        private LinearLayout EveryDayLinearLayout;
+
+        /// <summary>
+        /// Overriding on create method of activity to custumize the dashboard behaviour
+        /// </summary>
+        /// <param name="savedInstanceState">saved intent from activity</param>
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            // Set our view from the "main" layout resource
+            this.currentDate = DateTime.Today;
+
+            //// Set our view from the "DashboardPage" layout resource
             SetContentView(Resource.Layout.DashboardPage);
+
+            ////Set the actions to the date picker
             _dateSelectButton = FindViewById<Button>(Resource.Id.BtnCalendar);
             _dateSelectButton.Click += DateSelect_OnClick;
+            _dateSelectButton.Text = currentDate.ToShortDateString();
+
+            ////set the actions to the spinner
+            spinner = FindViewById<Spinner>(Resource.Id.spinnerLocation);
+            spinner.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
+            var adapter = ArrayAdapter.CreateFromResource(this, Resource.Array.LocationArray, Android.Resource.Layout.SimpleSpinnerItem);
+            adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+            spinner.Adapter = adapter;
+
+            ////set the actions to the selected button
+            SelectedButton = FindViewById<Button>(Resource.Id.BtnSelected);
+            SelectedButton.Click += SelectedButton_OnClick;
+            //SelectedButton.Text = "25&#10;" + Resources.GetString(Resource.String.SelectedCandidate);
+
+            ////set the actions to the To be determined button
+            TBDButton = FindViewById<Button>(Resource.Id.BtnTBD);
+            TBDButton.Click += TBDButton_OnClick;
+            //TBDButton.Text = "02&#10;" + Resources.GetString(Resource.String.TBDCandidate);
+
+            ////set the actions to the rejected button
+            RejectedButton = FindViewById<Button>(Resource.Id.BtnRejected);
+            RejectedButton.Click += RejectedButton_OnClick;
+            //RejectedButton.Text = "0&#10;" + Resources.GetString(Resource.String.RejectedCandidate);
+
+            //EveryDayLinearLayout = FindViewById<LinearLayout>(Resource.Id)
+
+            ////set the actions to the current date textview
+            TxtViewCurrentDate = FindViewById<TextView>(Resource.Id.TxtCurrentDate);
+            TxtViewCurrentDate.Text = currentDate.ToLongDateString();
         }
+
+        /// <summary>
+        /// handles the event of selected candidate clicked
+        /// </summary>
+        /// <param name="sender">selected button</param>
+        /// <param name="e">event arguments</param>
+        private void RejectedButton_OnClick(object sender, EventArgs e)
+        {
+            //StartActivity(typeof(SelectedActivity));
+        }
+
+        /// <summary>
+        /// handles the event of to be determined candidate clicked
+        /// </summary>
+        /// <param name="sender">to be determined button</param>
+        /// <param name="e">event arguments</param>
+        private void TBDButton_OnClick(object sender, EventArgs e)
+        {
+            
+        }
+
+        /// <summary>
+        /// handles the event of rejected candidate clicked
+        /// </summary>
+        /// <param name="sender">rejected button</param>
+        /// <param name="e">event arguments</param>
+        private void SelectedButton_OnClick(object sender, EventArgs e)
+        {
+            
+        }
+
+        /// <summary>
+        /// handles the event of date selection from the date picker
+        /// </summary>
+        /// <param name="sender">date picker</param>
+        /// <param name="e">event arguments</param>
         private void DateSelect_OnClick(object sender, EventArgs e)
         {
+            ////create instance of date picker fragment and pass the selected date to the front end
             DatePickerFragment frag = DatePickerFragment.NewInstance(delegate (DateTime date)
             {
                 _dateSelectButton.Text = date.ToShortDateString();
@@ -34,27 +119,68 @@ namespace FundooWalkin
 
             frag.Show(FragmentManager, DatePickerFragment.TAG);
         }
+
+        /// <summary>
+        /// handling the event of spinner item selected
+        /// </summary>
+        /// <param name="sender">sender spinner</param>
+        /// <param name="e">item selected event arguments</param>
+        private void spinner_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
+        {
+            Spinner spinner = (Spinner)sender;
+
+            ////set the selected item to the spinner prompt
+            spinner.Prompt = spinner.GetItemAtPosition(e.Position).ToString();
+        }
+
+        /// <summary>
+        /// class to set the calendar dialog to select the date 
+        /// </summary>
         private class DatePickerFragment : DialogFragment, DatePickerDialog.IOnDateSetListener
         {
-            public static readonly string TAG = "X:" + typeof(DatePickerFragment).Name.ToUpper();
-
             //// Initialize this value to prevent NullReferenceExceptions.  
             Action<DateTime> _dateSelectedHandler = delegate { };
+
+            public static readonly string TAG = "X:" + typeof(DatePickerFragment).Name.ToUpper();
+
+            /// <summary>
+            /// set the date selection handler
+            /// </summary>
+            /// <param name="onDateSelected">date selected</param>
+            /// <returns>returns fragment</returns>
             public static DatePickerFragment NewInstance(Action<DateTime> onDateSelected)
             {
                 DatePickerFragment frag = new DatePickerFragment();
                 frag._dateSelectedHandler = onDateSelected;
                 return frag;
             }
+
+            /// <summary>
+            /// on create dialog to choose the date
+            /// </summary>
+            /// <param name="savedInstanceState">bundle from previous activity</param>
+            /// <returns>date dialog</returns>
             public override Dialog OnCreateDialog(Bundle savedInstanceState)
             {
+                ////get current date
                 DateTime currently = DateTime.Today;
+
+                ////pass to open the dialog on current date
+                //// Note: monthOfYear is a value between 0 and 11, not 1 and 12! 
                 DatePickerDialog dialog = new DatePickerDialog(Activity, this, currently.Year, currently.Month - 1, currently.Day);
                 return dialog;
             }
+
+            /// <summary>
+            /// set the date to the picker
+            /// </summary>
+            /// <param name="view">the calendar</param>
+            /// <param name="year">year of the selected date</param>
+            /// <param name="monthOfYear">month of selected date</param>
+            /// <param name="dayOfMonth">day of selected date</param>
             public void OnDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth)
             {
-                // Note: monthOfYear is a value between 0 and 11, not 1 and 12!  
+                //// Note: monthOfYear is a value between 0 and 11, not 1 and 12!  
                 DateTime selectedDate = new DateTime(year, monthOfYear + 1, dayOfMonth);
                 _dateSelectedHandler(selectedDate);
             }
--- Resources/Resource.designer.cs
+++ Resources/Resource.designer.cs
@@ -3139,26 +3139,26 @@ namespace FundooWalkin
 			// aapt resource value: 0x7f020054
 			public const int avd_hide_password = 2130837588;
 			
-			// aapt resource value: 0x7f020073
-			public const int avd_hide_password_1 = 2130837619;
-			
 			// aapt resource value: 0x7f020074
-			public const int avd_hide_password_2 = 2130837620;
+			public const int avd_hide_password_1 = 2130837620;
 			
 			// aapt resource value: 0x7f020075
-			public const int avd_hide_password_3 = 2130837621;
+			public const int avd_hide_password_2 = 2130837621;
+			
+			// aapt resource value: 0x7f020076
+			public const int avd_hide_password_3 = 2130837622;
 			
 			// aapt resource value: 0x7f020055
 			public const int avd_show_password = 2130837589;
 			
-			// aapt resource value: 0x7f020076
-			public const int avd_show_password_1 = 2130837622;
-			
 			// aapt resource value: 0x7f020077
-			public const int avd_show_password_2 = 2130837623;
+			public const int avd_show_password_1 = 2130837623;
 			
 			// aapt resource value: 0x7f020078
-			public const int avd_show_password_3 = 2130837624;
+			public const int avd_show_password_2 = 2130837624;
+			
+			// aapt resource value: 0x7f020079
+			public const int avd_show_password_3 = 2130837625;
 			
 			// aapt resource value: 0x7f020056
 			public const int CalendarIcon = 2130837590;
@@ -3197,55 +3197,58 @@ namespace FundooWalkin
 			public const int LocationIcon = 2130837601;
 			
 			// aapt resource value: 0x7f020062
-			public const int mtrl_snackbar_background = 2130837602;
+			public const int locationIcon1 = 2130837602;
 			
 			// aapt resource value: 0x7f020063
-			public const int mtrl_tabs_default_indicator = 2130837603;
+			public const int mtrl_snackbar_background = 2130837603;
 			
 			// aapt resource value: 0x7f020064
-			public const int navigation_empty_icon = 2130837604;
+			public const int mtrl_tabs_default_indicator = 2130837604;
 			
 			// aapt resource value: 0x7f020065
-			public const int notification_action_background = 2130837605;
+			public const int navigation_empty_icon = 2130837605;
 			
 			// aapt resource value: 0x7f020066
-			public const int notification_bg = 2130837606;
+			public const int notification_action_background = 2130837606;
 			
 			// aapt resource value: 0x7f020067
-			public const int notification_bg_low = 2130837607;
+			public const int notification_bg = 2130837607;
 			
 			// aapt resource value: 0x7f020068
-			public const int notification_bg_low_normal = 2130837608;
+			public const int notification_bg_low = 2130837608;
 			
 			// aapt resource value: 0x7f020069
-			public const int notification_bg_low_pressed = 2130837609;
+			public const int notification_bg_low_normal = 2130837609;
 			
 			// aapt resource value: 0x7f02006a
-			public const int notification_bg_normal = 2130837610;
+			public const int notification_bg_low_pressed = 2130837610;
 			
 			// aapt resource value: 0x7f02006b
-			public const int notification_bg_normal_pressed = 2130837611;
+			public const int notification_bg_normal = 2130837611;
 			
 			// aapt resource value: 0x7f02006c
-			public const int notification_icon_background = 2130837612;
-			
-			// aapt resource value: 0x7f020071
-			public const int notification_template_icon_bg = 2130837617;
-			
-			// aapt resource value: 0x7f020072
-			public const int notification_template_icon_low_bg = 2130837618;
+			public const int notification_bg_normal_pressed = 2130837612;
 			
 			// aapt resource value: 0x7f02006d
-			public const int notification_tile_bg = 2130837613;
+			public const int notification_icon_background = 2130837613;
+			
+			// aapt resource value: 0x7f020072
+			public const int notification_template_icon_bg = 2130837618;
+			
+			// aapt resource value: 0x7f020073
+			public const int notification_template_icon_low_bg = 2130837619;
 			
 			// aapt resource value: 0x7f02006e
-			public const int notify_panel_notification_icon_bg = 2130837614;
+			public const int notification_tile_bg = 2130837614;
 			
 			// aapt resource value: 0x7f02006f
-			public const int tooltip_frame_dark = 2130837615;
+			public const int notify_panel_notification_icon_bg = 2130837615;
 			
 			// aapt resource value: 0x7f020070
-			public const int tooltip_frame_light = 2130837616;
+			public const int tooltip_frame_dark = 2130837616;
+			
+			// aapt resource value: 0x7f020071
+			public const int tooltip_frame_light = 2130837617;
 			
 			static Drawable()
 			{
@@ -3263,31 +3266,33 @@ namespace FundooWalkin
 			// aapt resource value: 0x7f0a0039
 			public const int ALT = 2131361849;
 			
-			// aapt resource value: 0x7f0a00a3
-			public const int BtnCalendar = 2131361955;
+			// aapt resource value: 0x7f0a00a4
+			public const int BtnCalendar = 2131361956;
+			
+			// aapt resource value: 0x7f0a00a7
+			public const int BtnRejected = 2131361959;
+			
+			// aapt resource value: 0x7f0a00a5
+			public const int BtnSelected = 2131361957;
+			
+			// aapt resource value: 0x7f0a00a6
+			public const int BtnTBD = 2131361958;
 			
 			// aapt resource value: 0x7f0a003a
 			public const int CTRL = 2131361850;
 			
+			// aapt resource value: 0x7f0a00ab
+			public const int CandidateSearchView = 2131361963;
+			
+			// aapt resource value: 0x7f0a00a8
+			public const int EveryDayLayout = 2131361960;
+			
 			// aapt resource value: 0x7f0a003b
 			public const int FUNCTION = 2131361851;
 			
-<<<<<<< HEAD
-			// aapt resource value: 0x7f0a00b1
-			public const int Loginbtn = 2131361969;
+			// aapt resource value: 0x7f0a00bb
+			public const int Loginbtn = 2131361979;
 			
-=======
-<<<<<<< HEAD
-			// aapt resource value: 0x7f0a00b1
-			public const int Loginbtn = 2131361969;
-			
-=======
-
-			// aapt resource value: 0x7f0a00af
-			public const int Loginbtn = 2131361967;
-
->>>>>>> 6523632f070c61bb9bb50ae2958434c8a5e14d6f
->>>>>>> 049db04890ea876a7664a3717c47d7a377489ae3
 			// aapt resource value: 0x7f0a003c
 			public const int META = 2131361852;
 			
@@ -3296,6 +3301,12 @@ namespace FundooWalkin
 			
 			// aapt resource value: 0x7f0a003e
 			public const int SYM = 2131361854;
+			
+			// aapt resource value: 0x7f0a00aa
+			public const int SearchLinearLayout = 2131361962;
+			
+			// aapt resource value: 0x7f0a00a9
+			public const int TxtCurrentDate = 2131361961;
 			
 			// aapt resource value: 0x7f0a008d
 			public const int action_bar = 2131361933;
@@ -3318,17 +3329,17 @@ namespace FundooWalkin
 			// aapt resource value: 0x7f0a0069
 			public const int action_bar_title = 2131361897;
 			
-			// aapt resource value: 0x7f0a00b2
-			public const int action_container = 2131361970;
+			// aapt resource value: 0x7f0a00bc
+			public const int action_container = 2131361980;
 			
 			// aapt resource value: 0x7f0a008e
 			public const int action_context_bar = 2131361934;
 			
-			// aapt resource value: 0x7f0a00bd
-			public const int action_divider = 2131361981;
+			// aapt resource value: 0x7f0a00c7
+			public const int action_divider = 2131361991;
 			
-			// aapt resource value: 0x7f0a00b3
-			public const int action_image = 2131361971;
+			// aapt resource value: 0x7f0a00bd
+			public const int action_image = 2131361981;
 			
 			// aapt resource value: 0x7f0a0003
 			public const int action_menu_divider = 2131361795;
@@ -3345,11 +3356,11 @@ namespace FundooWalkin
 			// aapt resource value: 0x7f0a006b
 			public const int action_mode_close_button = 2131361899;
 			
-			// aapt resource value: 0x7f0a00b4
-			public const int action_text = 2131361972;
-			
 			// aapt resource value: 0x7f0a00be
-			public const int actions = 2131361982;
+			public const int action_text = 2131361982;
+			
+			// aapt resource value: 0x7f0a00c8
+			public const int actions = 2131361992;
 			
 			// aapt resource value: 0x7f0a006c
 			public const int activity_chooser_view_content = 2131361900;
@@ -3411,8 +3422,8 @@ namespace FundooWalkin
 			// aapt resource value: 0x7f0a0086
 			public const int checkbox = 2131361926;
 			
-			// aapt resource value: 0x7f0a00bc
-			public const int chronometer = 2131361980;
+			// aapt resource value: 0x7f0a00c6
+			public const int chronometer = 2131361990;
 			
 			// aapt resource value: 0x7f0a0061
 			public const int clip_horizontal = 2131361889;
@@ -3423,8 +3434,8 @@ namespace FundooWalkin
 			// aapt resource value: 0x7f0a0040
 			public const int collapseActionView = 2131361856;
 			
-			// aapt resource value: 0x7f0a00a6
-			public const int container = 2131361958;
+			// aapt resource value: 0x7f0a00ae
+			public const int container = 2131361966;
 			
 			// aapt resource value: 0x7f0a0082
 			public const int content = 2131361922;
@@ -3432,8 +3443,8 @@ namespace FundooWalkin
 			// aapt resource value: 0x7f0a0075
 			public const int contentPanel = 2131361909;
 			
-			// aapt resource value: 0x7f0a00a7
-			public const int coordinator = 2131361959;
+			// aapt resource value: 0x7f0a00af
+			public const int coordinator = 2131361967;
 			
 			// aapt resource value: 0x7f0a007c
 			public const int custom = 2131361916;
@@ -3447,20 +3458,20 @@ namespace FundooWalkin
 			// aapt resource value: 0x7f0a006f
 			public const int default_activity_button = 2131361903;
 			
-			// aapt resource value: 0x7f0a00a9
-			public const int design_bottom_sheet = 2131361961;
+			// aapt resource value: 0x7f0a00b1
+			public const int design_bottom_sheet = 2131361969;
 			
-			// aapt resource value: 0x7f0a00ae
-			public const int design_menu_item_action_area = 2131361966;
+			// aapt resource value: 0x7f0a00b6
+			public const int design_menu_item_action_area = 2131361974;
 			
-			// aapt resource value: 0x7f0a00ad
-			public const int design_menu_item_action_area_stub = 2131361965;
+			// aapt resource value: 0x7f0a00b5
+			public const int design_menu_item_action_area_stub = 2131361973;
 			
-			// aapt resource value: 0x7f0a00ac
-			public const int design_menu_item_text = 2131361964;
+			// aapt resource value: 0x7f0a00b4
+			public const int design_menu_item_text = 2131361972;
 			
-			// aapt resource value: 0x7f0a00ab
-			public const int design_navigation_view = 2131361963;
+			// aapt resource value: 0x7f0a00b3
+			public const int design_navigation_view = 2131361971;
 			
 			// aapt resource value: 0x7f0a0027
 			public const int disableHome = 2131361831;
@@ -3519,8 +3530,8 @@ namespace FundooWalkin
 			// aapt resource value: 0x7f0a0071
 			public const int icon = 2131361905;
 			
-			// aapt resource value: 0x7f0a00bf
-			public const int icon_group = 2131361983;
+			// aapt resource value: 0x7f0a00c9
+			public const int icon_group = 2131361993;
 			
 			// aapt resource value: 0x7f0a0041
 			public const int ifRoom = 2131361857;
@@ -3528,8 +3539,11 @@ namespace FundooWalkin
 			// aapt resource value: 0x7f0a006e
 			public const int image = 2131361902;
 			
-			// aapt resource value: 0x7f0a00b8
-			public const int info = 2131361976;
+			// aapt resource value: 0x7f0a00a2
+			public const int imageView1 = 2131361954;
+			
+			// aapt resource value: 0x7f0a00c2
+			public const int info = 2131361986;
 			
 			// aapt resource value: 0x7f0a0068
 			public const int italic = 2131361896;
@@ -3540,8 +3554,8 @@ namespace FundooWalkin
 			// aapt resource value: 0x7f0a004e
 			public const int labeled = 2131361870;
 			
-			// aapt resource value: 0x7f0a00a5
-			public const int largeLabel = 2131361957;
+			// aapt resource value: 0x7f0a00ad
+			public const int largeLabel = 2131361965;
 			
 			// aapt resource value: 0x7f0a0054
 			public const int left = 2131361876;
@@ -3558,8 +3572,8 @@ namespace FundooWalkin
 			// aapt resource value: 0x7f0a0070
 			public const int list_item = 2131361904;
 			
-			// aapt resource value: 0x7f0a00c1
-			public const int masked = 2131361985;
+			// aapt resource value: 0x7f0a00cb
+			public const int masked = 2131361995;
 			
 			// aapt resource value: 0x7f0a009c
 			public const int message = 2131361948;
@@ -3579,20 +3593,14 @@ namespace FundooWalkin
 			// aapt resource value: 0x7f0a002f
 			public const int multiply = 2131361839;
 			
-			// aapt resource value: 0x7f0a00b0
-			public const int myimg = 2131361968;
+			// aapt resource value: 0x7f0a00b8
+			public const int myimg = 2131361976;
 			
-<<<<<<< HEAD
-			// aapt resource value: 0x7f0a00aa
-			public const int navigation_header_container = 2131361962;
-=======
-			// aapt resource value: 0x7f0a00af
-			public const int name = 2131361967;
+			// aapt resource value: 0x7f0a00b9
+			public const int name = 2131361977;
 			
-			// aapt resource value: 0x7f0a00a8
-			public const int navigation_header_container = 2131361960;
-
->>>>>>> 049db04890ea876a7664a3717c47d7a377489ae3
+			// aapt resource value: 0x7f0a00b2
+			public const int navigation_header_container = 2131361970;
 			
 			// aapt resource value: 0x7f0a0042
 			public const int never = 2131361858;
@@ -3603,14 +3611,14 @@ namespace FundooWalkin
 			// aapt resource value: 0x7f0a0025
 			public const int normal = 2131361829;
 			
-			// aapt resource value: 0x7f0a00ba
-			public const int notification_background = 2131361978;
+			// aapt resource value: 0x7f0a00c4
+			public const int notification_background = 2131361988;
 			
-			// aapt resource value: 0x7f0a00b6
-			public const int notification_main_column = 2131361974;
+			// aapt resource value: 0x7f0a00c0
+			public const int notification_main_column = 2131361984;
 			
-			// aapt resource value: 0x7f0a00b5
-			public const int notification_main_column_container = 2131361973;
+			// aapt resource value: 0x7f0a00bf
+			public const int notification_main_column_container = 2131361983;
 			
 			// aapt resource value: 0x7f0a0060
 			public const int outline = 2131361888;
@@ -3624,8 +3632,8 @@ namespace FundooWalkin
 			// aapt resource value: 0x7f0a000b
 			public const int parent_matrix = 2131361803;
 			
-			// aapt resource value: 0x7f0a00b0
-			public const int password = 2131361968;
+			// aapt resource value: 0x7f0a00ba
+			public const int password = 2131361978;
 			
 			// aapt resource value: 0x7f0a0058
 			public const int pin = 2131361880;
@@ -3642,11 +3650,11 @@ namespace FundooWalkin
 			// aapt resource value: 0x7f0a0055
 			public const int right = 2131361877;
 			
-			// aapt resource value: 0x7f0a00b9
-			public const int right_icon = 2131361977;
+			// aapt resource value: 0x7f0a00c3
+			public const int right_icon = 2131361987;
 			
-			// aapt resource value: 0x7f0a00b7
-			public const int right_side = 2131361975;
+			// aapt resource value: 0x7f0a00c1
+			public const int right_side = 2131361985;
 			
 			// aapt resource value: 0x7f0a000c
 			public const int save_image_matrix = 2131361804;
@@ -3723,8 +3731,8 @@ namespace FundooWalkin
 			// aapt resource value: 0x7f0a002c
 			public const int showTitle = 2131361836;
 			
-			// aapt resource value: 0x7f0a00a4
-			public const int smallLabel = 2131361956;
+			// aapt resource value: 0x7f0a00ac
+			public const int smallLabel = 2131361964;
 			
 			// aapt resource value: 0x7f0a0016
 			public const int snackbar_action = 2131361814;
@@ -3741,8 +3749,8 @@ namespace FundooWalkin
 			// aapt resource value: 0x7f0a0073
 			public const int spacer = 2131361907;
 			
-			// aapt resource value: 0x7f0a00a2
-			public const int spinnerLocation = 2131361954;
+			// aapt resource value: 0x7f0a00a3
+			public const int spinnerLocation = 2131361955;
 			
 			// aapt resource value: 0x7f0a0008
 			public const int split_action_bar = 2131361800;
@@ -3795,8 +3803,8 @@ namespace FundooWalkin
 			// aapt resource value: 0x7f0a005a
 			public const int textStart = 2131361882;
 			
-			// aapt resource value: 0x7f0a00af
-			public const int text_input_password_toggle = 2131361967;
+			// aapt resource value: 0x7f0a00b7
+			public const int text_input_password_toggle = 2131361975;
 			
 			// aapt resource value: 0x7f0a0018
 			public const int textinput_counter = 2131361816;
@@ -3807,8 +3815,8 @@ namespace FundooWalkin
 			// aapt resource value: 0x7f0a001a
 			public const int textinput_helper_text = 2131361818;
 			
-			// aapt resource value: 0x7f0a00bb
-			public const int time = 2131361979;
+			// aapt resource value: 0x7f0a00c5
+			public const int time = 2131361989;
 			
 			// aapt resource value: 0x7f0a0023
 			public const int title = 2131361827;
@@ -3825,8 +3833,8 @@ namespace FundooWalkin
 			// aapt resource value: 0x7f0a007d
 			public const int topPanel = 2131361917;
 			
-			// aapt resource value: 0x7f0a00a8
-			public const int touch_outside = 2131361960;
+			// aapt resource value: 0x7f0a00b0
+			public const int touch_outside = 2131361968;
 			
 			// aapt resource value: 0x7f0a000f
 			public const int transition_current_scene = 2131361807;
@@ -3858,8 +3866,8 @@ namespace FundooWalkin
 			// aapt resource value: 0x7f0a001b
 			public const int view_offset_helper = 2131361819;
 			
-			// aapt resource value: 0x7f0a00c0
-			public const int visible = 2131361984;
+			// aapt resource value: 0x7f0a00ca
+			public const int visible = 2131361994;
 			
 			// aapt resource value: 0x7f0a0043
 			public const int withText = 2131361859;
@@ -4174,6 +4182,15 @@ namespace FundooWalkin
 		
 		public partial class String
 		{
+			
+			// aapt resource value: 0x7f0d003a
+			public const int RejectedCandidate = 2131558458;
+			
+			// aapt resource value: 0x7f0d0038
+			public const int SelectedCandidate = 2131558456;
+			
+			// aapt resource value: 0x7f0d0039
+			public const int TBDCandidate = 2131558457;
 			
 			// aapt resource value: 0x7f0d0000
 			public const int abc_action_bar_home_description = 2131558400;
